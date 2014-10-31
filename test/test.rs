6751b7517ebeb62cbbee64fca510efd44b96675a
Damien Schoof <damien.schoof@gmail.com>
2014-10-31 09:34:26 -0400

    Fix fail! that I missed.

--- test/test.rs
+++ test/test.rs
@@ -10,7 +10,7 @@ fn test_index() {
 
     let mut file = match File::create(&Path::new("test/index.actual.html")) {
         Ok(f) => f,
-        Err(e) => fail!("Unable to create file `index.actual.html`: {}", e)
+        Err(e) => panic!("Unable to create file `index.actual.html`: {}", e)
     };
 
     let model = vec! [(0, "First".to_string()), (1, "Second item".to_string())];
